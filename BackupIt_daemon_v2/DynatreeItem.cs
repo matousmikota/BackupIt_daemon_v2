8f4e8359bb041a8bd51b2a048b2c289aa0721db4
matousmikota <33751481+matousmikota@users.noreply.github.com>
2022-03-30 18:34:12 +0200

    Added DynatreeItem class -Converting a directory structure and parsing to JSON format https://stackoverflow.com/questions/24725775/converting-a-directory-structure-and-parsing-to-json-format-in-c-sharp

---
+++ BackupIt_daemon_v2/DynatreeItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BackupIt_daemon_v2
+{
+    public class DynatreeItem
+    {
+        public string title { get; set; }
+        public bool isFolder { get; set; }
+        public string key { get; set; }
+        public string modified { get; set; }
+        public List<DynatreeItem> children;
+        public DynatreeItem(FileSystemInfo fsi)
+        {
+            title = fsi.Name;
+            modified = fsi.LastWriteTime.ToString("yyyyMMddHHmmss");
+            children = new List<DynatreeItem>();
+            if (fsi.Attributes == FileAttributes.Directory)
+            {
+                isFolder = true;
+                foreach (FileSystemInfo f in (fsi as DirectoryInfo).GetFileSystemInfos())
+                {
+                    children.Add(new DynatreeItem(f));
+                }
+            }
+            else
+            {
+                isFolder = false;
+            }
+            key = title.Replace(" ", "").ToLower();
+        }
+        
+        public string JsonToDynatree()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
---
+++ BackupIt_daemon_v2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace BackupIt_daemon_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DynatreeItem di = new DynatreeItem(new DirectoryInfo(@"C:\Users\a\Music\source-testA"));
+            string result = "[" + di.JsonToDynatree() + "]";
+            Console.WriteLine(result);
+        }
+    }
+}
