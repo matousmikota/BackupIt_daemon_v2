4ce74f819facfa052d4e99de3f41bde15ff9981d
matousmikota <33751481+matousmikota@users.noreply.github.com>
2022-04-03 14:40:13 +0200

    Compare two JSON files and output differences into a JSON file.

---
+++ BackupIt_daemon_v2/Snapshoter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupIt_daemon_v2
+{
+    public class Snapshoter
+    {
+        public void Create(string folderPath, string snapshopFilePath)
+        {
+            DynatreeItem di = new DynatreeItem(new DirectoryInfo(folderPath));
+            string snapshotText = "[" + di.JsonToDynatree() + "]";
+
+            using (StreamWriter writer = new(snapshopFilePath))
+            {
+                writer.Write(snapshotText);
+            }
+        }
+
+    }
+}
