72eeea64894cc2d1848eb1dc6c63636fb71c80ec
matousmikota <33751481+matousmikota@users.noreply.github.com>
2022-04-12 14:54:39 +0200

    Updated snapshoting procedure

---
+++ BackupIt_daemon_v2/FileSystemNode.cs
@@ -0,0 +1,10 @@
+﻿namespace BackupIt_daemon_v2
+{
+    public class FileSystemNode
+    {
+        public string FullPath { get; set; }
+        public string LastModified { get; set; }
+        public string Action { get; set; }
+        public bool IsDirectory { get; set; }
+    }
+}
--- BackupIt_daemon_v2/Snapshoter.cs
+++ BackupIt_daemon_v2/Snapshoter.cs
@@ -4,21 +4,50 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace BackupIt_daemon_v2
 {
     public class Snapshoter
     {
-        public void Create(string folderPath, string snapshopFilePath)
-        {
-            DynatreeItem di = new DynatreeItem(new DirectoryInfo(folderPath));
-            string snapshotText = "[" + di.JsonToDynatree() + "]";
+        public List<FileSystemNode> Nodes { get; set; } = new();
 
-            using (StreamWriter writer = new(snapshopFilePath))
+        public Snapshoter(string sourcePath, string outputPath)
+        {
+            this.Create(new DirectoryInfo(sourcePath));
+
+            using (StreamWriter sw = new(outputPath))
             {
-                writer.Write(snapshotText);
+                sw.Write(this.NodesToJson());
             }
         }
 
+        private void Create(FileSystemInfo fsi)
+        {
+            FileSystemNode fsn = new();
+            fsn.FullPath = fsi.FullName;
+            fsn.LastModified = fsi.LastWriteTime.ToString("yyyyMMddHHmmss");
+
+            if (fsi.Attributes == FileAttributes.Directory)
+            {
+                fsn.IsDirectory = true;
+                this.Nodes.Add(fsn);
+
+                foreach (FileSystemInfo f in (fsi as DirectoryInfo).GetFileSystemInfos())
+                {
+                    this.Create(f);
+                }
+            }
+            else
+            {
+                fsn.IsDirectory = false;
+                this.Nodes.Add(fsn);
+            }
+        }
+
+        private string NodesToJson()
+        {
+            return JsonConvert.SerializeObject(this.Nodes, Formatting.None);
+        }
     }
 }
