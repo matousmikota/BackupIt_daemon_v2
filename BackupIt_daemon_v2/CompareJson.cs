05929fbb1b7ae25ae97f584081eb04dd9140110a
matousmikota <33751481+matousmikota@users.noreply.github.com>
2022-04-28 22:07:17 +0200

    Added backuping functionality (NOT TESTED, MIGHT NOT FULLY WORK YET). Does not create metadata yet.

--- BackupIt_daemon_v2/CompareJson.cs
+++ BackupIt_daemon_v2/CompareJson.cs
@@ -7,22 +7,68 @@ using System.Threading.Tasks;
 using Microsoft.XmlDiffPatch;
 using Newtonsoft.Json;
 using System.Xml;
-using JsonDiffPatchDotNet;
+//using JsonDiffPatchDotNet;
 using JsonDiffPatchDotNet.Formatters;
+using Newtonsoft.Json.Linq;
+
 
 namespace BackupIt_daemon_v2
 {
     public class CompareJson
     {
 
-        JsonDiffPatch jsonDiffPatch { get; set; } = new JsonDiffPatch();
+        //JsonDiffPatch jsonDiffPatch { get; set; } = new JsonDiffPatch();
 
+        /*
         public void Compare(string oldJson, string newJson, string snapshopFilePath)
         {
             using (StreamWriter writer = new(snapshopFilePath))
             {
                 writer.Write(jsonDiffPatch.Diff(oldJson, newJson));
             }
+        }*/
+
+        public void Compare(string leftPath, string rightPath)
+        {
+            List<FileSystemNode> leftNodes = JsonToList(leftPath);
+            List<FileSystemNode> rightNodes = JsonToList(rightPath);
+
+            List<FileSystemNode> added = rightNodes.Except(leftNodes).ToList();
+            List<FileSystemNode> removed = leftNodes.Except(rightNodes).ToList(); //TODO: Could do this in the foreach. When rightNodes.Find returns null.
+
+            List<FileSystemNode> modified = new();
+
+            foreach (FileSystemNode leftNode in leftNodes)
+            {
+                FileSystemNode rightNode = rightNodes.Find(x => x.FullPath == leftNode.FullPath);
+                DateTime rightDate = DateTime.Parse(rightNode.LastModified);
+                DateTime leftDate = DateTime.Parse(leftNode.LastModified);
+
+                if (DateTime.Compare(leftDate, rightDate) > 0)
+                {
+                    modified.Add(rightNode);
+                }
+            }
+
+        }
+
+        private List<FileSystemNode> JsonToList(string path)
+        {
+            string json;
+            using (StreamReader sr = new(path))
+            {
+                json = sr.ReadToEnd();
+            }
+
+            JArray jArray = (JArray)JsonConvert.DeserializeObject(json);
+
+            List<FileSystemNode> nodes = new();
+            foreach (JToken item in jArray)
+            {
+                nodes.Add(item.ToObject<FileSystemNode>());
+            }
+
+            return nodes;
         }
 
         /*
